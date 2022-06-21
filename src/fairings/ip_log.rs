11656a373a76b3b4f2158bd635fb100c887a6d04
Ilya Maximov <mail@overlisted.net>
2022-06-21 21:47:37 +0200

    fix logging with proxied ips

--- src/fairings/ip_log.rs
+++ src/fairings/ip_log.rs
@@ -1,5 +1,7 @@
 //! cry about it
 
+use std::net;
+
 use super::prelude::*;
 
 struct IpLogger;
@@ -15,13 +17,21 @@ impl Fairing for IpLogger {
 
     async fn on_request(&self, req: &mut rocket::Request<'_>, _data: &mut rocket::Data<'_>) {
         let db = Db::get_one(req.rocket()).await.unwrap();
+        let path = req.uri().path().to_string();
 
-        if let Some(addr) = req.remote() {
-            let path = req.uri().path().to_string();
+        let option = if let Some(value) = req.headers().get_one("X-Real-IP") {
+            value.parse::<net::IpAddr>().ok()
+        } else if let Some(addr) = req.remote() {
+            Some(addr.ip())
+        } else {
+            None
+        };
+
+        if let Some(addr) = option {
             db.run(move |conn| {
                 diesel::insert_into(schema::ip_log::table)
                     .values((
-                        schema::ip_log::addr.eq(ipnetwork::IpNetwork::from(addr.ip())),
+                        schema::ip_log::addr.eq(ipnetwork::IpNetwork::from(addr)),
                         schema::ip_log::path.eq(path),
                     ))
                     .execute(conn)
